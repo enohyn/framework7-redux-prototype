35746e3bb4f67cd9ab265bfd80f31a499df934cc
enohyn <shayokullah@gmail.com>
2023-08-16 16:44:12 +0600

    Introducing actioncreator with Asynchronus calling

--- src/redux/actions/product-actions.js
+++ src/redux/actions/product-actions.js
@@ -1,4 +1,6 @@
-import { REQUEST_PRODUCTS, RECIEVED_PRODUCTS, RECIEVE_FAILED, RECIEVE_SINGLE_PRODUCT } from "../constants/action-types"
+import axios from "axios"
+import { REQUEST_PRODUCTS, RECIEVED_PRODUCTS, RECIEVE_FAILED, RECIEVE_SINGLE_PRODUCT, FETCH_PRODUCTS } from "../constants/action-types"
+import { productsApi } from "../../apis/productsApi"
 
 export const requestProducts = () => {
     return {
@@ -6,12 +8,22 @@ export const requestProducts = () => {
     }
 }
 
-export const recievedProducts = (products) => {
-    return {
-        type: RECIEVED_PRODUCTS,
-        payload: products
-    }
+export const fetchProducts = () => async (dispatch) => {
+    const response = await productsApi.get('/products')
+    dispatch({ type: FETCH_PRODUCTS, payload: response.data.products })
 }
+
+export const fetchProduct =(id)=> async (dispatch)=>{
+    const response = await productsApi.get(`/products/${id}`) 
+    dispatch({type:RECIEVE_SINGLE_PRODUCT, payload:response.data})
+}
+
+// export const recievedProducts = (products) => {
+//     return {
+//         type: RECIEVED_PRODUCTS,
+//         payload: products
+//     }
+// }
 
 export const productRecieveFailed = (error) => {
     return {
@@ -20,9 +32,9 @@ export const productRecieveFailed = (error) => {
     }
 }
 
-export const selectedProduct = (product) => {
-    return {
-        type: RECIEVE_SINGLE_PRODUCT,
-        payload: product
-    }
-}
+// export const selectedProduct = (product) => {
+//     return {
+//         type: RECIEVE_SINGLE_PRODUCT,
+//         payload: product
+//     }
+// }
