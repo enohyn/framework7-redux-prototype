72fbb5edb405c20c96ac5133cf0ff856d0fbb832
enohyn <shayokullah@gmail.com>
2023-08-16 16:46:09 +0600

    thunk middleware introduced

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,13 @@
-import { legacy_createStore } from "redux";
+import { legacy_createStore, applyMiddleware, compose } from "redux";
 import { reducers } from "./reducers";
+import thunk from "redux-thunk";
 
-const store = legacy_createStore(reducers, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//before introducing middleware-thunk
+// const store = legacy_createStore(reducers, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+const store = legacy_createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 export default store;
